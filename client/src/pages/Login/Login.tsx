d0a99cd92ca96fc08b98ae0684c622a0b77cb984
vonbrank <vonbrank@outlook.com>
2024-03-03 01:01:56 +0800

    feat(client): 添加登录页面基础布局

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box
+      sx={{
+        background: "linear-gradient(to left, #71B280, #134E5E)",
+      }}
+    >
+      <Container
+        sx={{
+          minHeight: "100vh",
+        }}
+      >
+        <Stack
+          sx={{ minHeight: "inherit", paddingY: "4.8rem" }}
+          justifyContent={"center"}
+        >
+          <Paper sx={{ overflow: "hidden" }}>
+            <Stack direction={"row"}>
+              <Box sx={{ flex: 1, width: 0 }}>
+                <Box paddingX={"6.4rem"} paddingY={"6.4rem"}>
+                  <Typography textAlign={"center"} variant="h5">
+                    MGO Experience
+                  </Typography>
+
+                  <Typography
+                    textAlign={"center"}
+                    variant="h6"
+                    marginTop={"4.8rem"}
+                  >
+                    Sign In your account
+                  </Typography>
+
+                  <Stack spacing={"3rem"} marginTop={"3.6rem"}>
+                    <Stack spacing={"1.2rem"}>
+                      <Typography variant="subtitle1">Email</Typography>
+                      <TextField fullWidth />
+                    </Stack>
+                    <Stack spacing={"1.2rem"}>
+                      <Typography variant="subtitle1">Password</Typography>
+                      <TextField fullWidth />
+                    </Stack>
+                  </Stack>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    sx={{ marginTop: "3.6rem" }}
+                  >
+                    SIGN IN
+                  </Button>
+                </Box>
+              </Box>
+              <Stack sx={{ flex: 1, width: 0 }}>
+                <img
+                  style={{
+                    objectFit: "cover",
+                    flex: 1,
+                    objectPosition: "left",
+                  }}
+                  src="https://i.ytimg.com/vi/xFEK6Xc3pBA/sddefault.jpg"
+                />
+              </Stack>
+            </Stack>
+          </Paper>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
 
 export default Login;
